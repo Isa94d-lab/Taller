45861c6bd241b6bbda3597d73d02cee995767194
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2025-04-11 02:17:43 -0500

    ⚡️ Eliminacion de *
    Se eliminaron * de las consultas

--- Taller/Consultas.sql
+++ Taller/Consultas.sql
@@ -3,52 +3,54 @@
 USE vtaszfs;
 
 -- 1. Seleccionar todos los productos con precio mayor a $50
-SELECT * 
+SELECT id, nombre, precio, proveedor_id, tipo_id
 FROM Productos 
 WHERE precio > 50;
 
 
+
 -- 2. Consultar clientes registrados en una ciudad específica
-SELECT c.* 
-FROM Clientes c
-JOIN Ubicaciones u ON c.id = u.entidad_id
-WHERE u.tipo_entidad = 'Cliente' 
-AND u.ciudad = 'Ciudad_Especifica';
+SELECT Clientes.nombre, Clientes.email, Direcciones.ciudad
+FROM Clientes
+INNER JOIN ClientesDireccion ON Clientes.id = ClientesDireccion.cliente_id
+INNER JOIN Direcciones ON ClientesDireccion.direccion_id = Direcciones.id
+WHERE Direcciones.ciudad = 'Bogotá' ;
 
 -- 3. Mostrar empleados contratados en los últimos 2 años
-SELECT * 
-FROM DatosEmpleados 
+SELECT nombre, salario, fecha_contratacion
+FROM DatosEmpleado
 WHERE fecha_contratacion >= DATE_SUB(CURDATE(), INTERVAL 2 YEAR);
 
 -- 4. Seleccionar proveedores que suministran más de 5 productos
-SELECT p.id, p.nombre, COUNT(prod.id) AS total_productos
-FROM Proveedores p
-JOIN Productos prod ON p.id = prod.proveedor_id
-GROUP BY p.id
+SELECT Proveedores.id, Proveedores.nombre, COUNT(Productos.id) AS total_productos
+FROM Proveedores 
+INNER JOIN Productos ON Proveedores.id = Productos.proveedor_id
+GROUP BY Proveedores.id, Proveedores.nombre
 HAVING total_productos > 5;
 
 -- 5. Listar clientes que no tienen dirección registrada en Ubicaciones
-SELECT c.*
-FROM Clientes c
-LEFT JOIN Ubicaciones u ON c.id = u.entidad_id AND u.tipo_entidad = 'Cliente'
-WHERE u.id IS NULL;
+SELECT Clientes.id, Clientes.nombre, Clientes.email
+FROM Clientes 
+LEFT JOIN ClientesDireccion ON Clientes.id = ClientesDireccion.cliente_id
+WHERE ClientesDireccion.id IS NULL;
 
 -- 6. Calcular el total de ventas por cada cliente
-SELECT c.id, c.nombre, SUM(p.total) AS total_ventas
-FROM Clientes c
-JOIN Pedidos p ON c.id = p.cliente_id
-GROUP BY c.id, c.nombre;
+SELECT Clientes.id, Clientes.nombre, SUM(Pedidos.total) AS total_ventas
+FROM Clientes
+JOIN Pedidos ON Clientes.id = Pedidos.cliente_id
+GROUP BY Clientes.id, Clientes.nombre;
+
 
 -- 7. Mostrar el salario promedio de los empleados
 SELECT AVG(salario) AS salario_promedio 
-FROM DatosEmpleados;
+FROM DatosEmpleado;
 
 -- 8. Consultar el tipo de productos disponibles en TiposProductos
 SELECT tipo_nombre, descripcion 
 FROM TiposProductos;
 
 -- 9. Seleccionar los 3 productos más caros
-SELECT * 
+SELECT id, nombre, precio proveedor_id, tipo_id
 FROM Productos 
 ORDER BY precio DESC 
 LIMIT 3;
---
+++ Taller/dml.sql
@@ -0,0 +1,10 @@
+INSERT INTO Clientes (nombre, email) VALUES
+('Carlos Méndez', 'carlos.mendez@email.com'),
+('Laura Gómez', 'laura.gomez@email.com');
+
+
+INSERT INTO Proveedores (nombre) VALUES
+('Suministros Industriales S.A.S.'),
+('Comercializadora Eléctrica LTDA');
+
+
