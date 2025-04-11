ba8abeef1bfd40f1d6155c3f104ff1b4cd461712
Isa94d-lab <181164074+Isa94d-lab@users.noreply.github.com>
2025-04-11 02:48:29 -0500

    ⚡️ Eliminacion de *
    Se eliminaron los * de las subconsultas

--- Taller/Subconsultas.sql
+++ Taller/Subconsultas.sql
@@ -1,8 +1,9 @@
 USE vtaszfs;
 
 -- 1. Consultar el producto más caro en cada categoría.
-SELECT * FROM Productos p
-WHERE precio = (SELECT MAX(precio) FROM Productos WHERE tipo_id = p.tipo_id);
+SELECT nombre, precio, proveedor_id, tipo_id
+FROM Productos
+WHERE Productos.precio = (SELECT MAX(Productos.precio) FROM Productos WHERE tipo_id = Productos.tipo_id);
 
 -- 2. Encontrar el cliente con mayor total en pedidos.
 SELECT cliente_id, SUM(total) AS total_pedidos
@@ -10,10 +11,11 @@ FROM Pedidos
 GROUP BY cliente_id
 HAVING total_pedidos = (SELECT MAX(total_pedidos) 
                         FROM (SELECT cliente_id, SUM(total) AS total_pedidos 
-                              FROM Pedidos GROUP BY cliente_id) AS subquery);
+                              FROM Pedidos GROUP BY cliente_id) AS cliente_pedidos);
 
 -- 3. Listar empleados que ganan más que el salario promedio.
-SELECT * FROM Empleados 
+SELECT id, nombre, puesto, salario, fecha_contratacion
+FROM Empleados 
 WHERE salario > (SELECT AVG(salario) FROM Empleados);
 
 -- 4. Consultar productos que han sido pedidos más de 5 veces.
@@ -23,7 +25,8 @@ GROUP BY producto_id
 HAVING total_pedidos > 5;
 
 -- 5. Listar pedidos cuyo total es mayor al promedio de todos los pedidos.
-SELECT * FROM Pedidos
+SELECT id, cliente_id, fecha, total, empleado_id 
+FROM Pedidos
 WHERE total > (SELECT AVG(total) FROM Pedidos);
 
 -- 6. Seleccionar los 3 proveedores con más productos.
@@ -34,8 +37,9 @@ ORDER BY total_productos DESC
 LIMIT 3;
 
 -- 7. Consultar productos con precio superior al promedio en su tipo.
-SELECT * FROM Productos p
-WHERE precio > (SELECT AVG(precio) FROM Productos WHERE tipo_id = p.tipo_id);
+SELECT id, nombre, precio, proveedor_id, tipo_id
+FROM Productos
+WHERE precio > (SELECT AVG(precio) FROM Productos WHERE tipo_id = Productos.tipo_id);
 
 -- 8. Mostrar clientes que han realizado más pedidos que la media.
 SELECT Pedidos.cliente_id, Clientes.nombre,  COUNT(*) AS total_pedidos
@@ -47,11 +51,13 @@ HAVING total_pedidos > (SELECT AVG(total_pedidos)
                               FROM Pedidos GROUP BY cliente_id) AS promedio);
 
 -- 9. Encontrar productos cuyo precio es mayor que el promedio de todos los productos.
-SELECT * FROM Productos 
+SELECT id, nombre, precio, proveedor_id, tipo_id
+FROM Productos 
 WHERE precio > (SELECT AVG(precio) FROM Productos);
 
 -- 10. Mostrar empleados cuyo salario es menor al promedio del departamento.
-SELECT * FROM Empleados e
+SELECT id, nombre, puesto, salario, fecha_contratacion
+FROM Empleados 
 WHERE salario < (SELECT AVG(salario) 
-                 FROM Empleados e2 
-                 WHERE e2.puesto = e.puesto);
+                 FROM Empleados as Empleados2
+                 WHERE Empleados2.puesto = Empleados.puesto);
